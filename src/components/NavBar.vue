41563796788dfc3d26faf5563d954b1e48f0ca0b
KeiraKistensamy <Keirakist0987@gmail.com>
2024-06-26 11:26:50 +0200

    navbarSetup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <router-link class="navbar-brand" to="/"><img src="https://keirakistensamy.github.io/allimages/images/KeiraLogo.jpg" alt="logo"></router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto me- mb-2 mb-lg-0">
+                <li class="nav-item"><router-link class="nav-link active" to="/">Home</router-link></li>
+                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="#resume">Resume</a></li>
+                <li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li>
+                <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                <li class="nav-item"><a class="nav-link" href="#testimonials">Testimonials</a></li>
+                <li class="nav-item"><a class="nav-link" href="#reachme">Reach Me</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
