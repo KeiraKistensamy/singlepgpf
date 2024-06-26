414bf7f9666a2bca7d47fe2add81f1754f2ebf45
KeiraKistensamy <Keirakist0987@gmail.com>
2024-06-26 11:41:12 +0200

    navbarSetup&footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="container-fluid">
+    <div class="row">
+        <p class="lead">Keira's Portfolio &copy;  <span class="shadow">{{cYear}}</span>
+        </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "FooterComp",
+    data() {
+        return {
+            cYear: new Date().getUTCFullYear()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
